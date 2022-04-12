c5e68de5d6d7a8c052e82bf0908182bb1d38b1eb
Dmitry Kirdyashkin <dikirdya@mts.ru>
2022-04-13 00:18:28 +0300

    test

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -17,68 +17,68 @@ resource "yandex_vpc_subnet" "app-subnet" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  image_id        = var.image_id
-  subnet_id       = yandex_vpc_subnet.app-subnet.id
-}
-
-module "db" {
-  source          = "../modules/db"
-  public_key_path = var.public_key_path
-  image_id        = var.image_id
-  subnet_id       = yandex_vpc_subnet.app-subnet.id
-}
-
-# resource "yandex_compute_instance" "app" {
-#   count = var.instance_count
-#   name = "reddit-app-${count.index}"
-#   zone = var.app_zone
-
-#   resources {
-#     cores  = 2
-#     memory = 2
-#   }
-
-#   boot_disk {
-#     initialize_params {
-#       # ”казать id образа созданного в предыдущем домашем задании
-#       image_id = var.image_id
-#     }
-#   }
-
-#   # network_interface {
-#   #   # ”казан id подсети default-ru-central1-a
-#   #   subnet_id = var.subnet_id
-#   #   nat       = true
-#   # }
-
-#   network_interface {
-#     subnet_id = yandex_vpc_subnet.app-subnet.id
-#     nat = true
-#   }
-
-#   metadata = {
-#     ssh-keys = "ubuntu:${file(var.public_key_path)}"
-#   }
-
-#   connection {
-#     type  = "ssh"
-#     host  = self.network_interface.0.nat_ip_address
-#     user  = "ubuntu"
-#     agent = false
-#     # путь до приватного ключа
-#     private_key = file(var.private_key_path)
-#   }
-
-#   provisioner "file" {
-#     source      = "files/puma.service"
-#     destination = "/tmp/puma.service"
-#   }
-
-#   provisioner "remote-exec" {
-#     script = "files/deploy.sh"
-#   }
-
+# module "app" {
+#   source          = "../modules/app"
+#   public_key_path = var.public_key_path
+#   image_id        = var.image_id
+#   subnet_id       = yandex_vpc_subnet.app-subnet.id
 # }
+
+# module "db" {
+#   source          = "../modules/db"
+#   public_key_path = var.public_key_path
+#   image_id        = var.image_id
+#   subnet_id       = yandex_vpc_subnet.app-subnet.id
+# }
+
+resource "yandex_compute_instance" "app" {
+  count = var.instance_count
+  name = "reddit-app-${count.index}"
+  zone = var.app_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # ”казать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+    }
+  }
+
+  # network_interface {
+  #   # ”казан id подсети default-ru-central1-a
+  #   subnet_id = var.subnet_id
+  #   nat       = true
+  # }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.app-subnet.id
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+}
